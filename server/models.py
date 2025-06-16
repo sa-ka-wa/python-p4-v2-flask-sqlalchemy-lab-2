374f29f48af77118c977c4885d718f5a0fc645ab
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-06-16 09:39:42 +0300

    added Association Proxy and Serialization

--- server/models.py
+++ server/models.py
@@ -11,20 +11,26 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
+
+    serialize_rules = ('-reviews.customer',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
     reviews = db.relationship('Review', back_populates = 'customer')
 
+    items = association_proxy('reviews', 'item',
+        creator=lambda item: Review(item=item))
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
+    serialize_rules = ('-reviews.item',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -35,8 +41,10 @@ class Item(db.Model):
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
 
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
+
+    serialize_rules = ('-customer.reviews','-item.reviews',)
 
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String)
